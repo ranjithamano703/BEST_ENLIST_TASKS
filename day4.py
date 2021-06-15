c3b5360624b0bd3fb9eb52966d7355c3cbae7beb
ranjithamano703 <84843896+ranjithamano703@users.noreply.github.com>
2021-06-15 14:35:15 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,17 @@
+a=b=c=90
+print(a/10)
+print(b*50)
+print(c+60)
+s="clock"
+print(s.replace("o","G"))
+a=12
+b=13.5
+print("a=",a,"before converting, datatype of a:",type(a))
+print("b=",b,"before converting, datatype of b:",type(b))
+a=float(a)
+print("a=",a,"after converting, datatype of a:",type(a))
+b=int(b)
+print("b=",b,"after converting, datatype of b:",type(b))
+
+
+
